ea9da33a5f2b40e36d46dae5919c305a35b53fe4
Elisabeth Jessica Valencia <elisabethtsy29@gmail.com>
2019-05-16 10:18:33 +0700

    Initial Commit

---
+++ LatihanDataFlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LatihanDataFlow
+//
+//  Created by Elisabeth Jessica Valencia on 16/05/19.
+//  Copyright © 2019 Elisabeth Jessica Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
